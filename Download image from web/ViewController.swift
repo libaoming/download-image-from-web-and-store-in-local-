8d260d1264d838feff6ea1c9381bdbb81d03f619
master <libaoming2@gmail.com>
2016-08-25 21:32:58 +0800

    load image from web and store image into file system

--- Download image from web/ViewController.swift
+++ Download image from web/ViewController.swift
@@ -10,16 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bachImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadFileImage()
+        
+        
+}
+    
+    func loadFileImage(){
+        
+        let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        
+        if documentPath.count > 0 {
+            
+            let documentDir = documentPath[0]
+            
+            let restorePath = documentDir + "/bach.jpg"
+            
+            bachImage.image = UIImage(contentsOfFile: restorePath)
+            
+            
+        
+        }
+
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func loadImage(){
+        
+        let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Johann_Sebastian_Bach.jpg/260px-Johann_Sebastian_Bach.jpg")
+        
+        let request = URLRequest(url: url!)
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            data,response,error in
+            
+            if error != nil {
+                print(error)
+            }else {
+                
+                if let dataTemp = data {
+                    
+                    self.bachImage.image = UIImage(data: dataTemp)
+                    
+                    let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+                    
+                    if documentPath.count > 0 {
+                        
+                        let documentDir = documentPath[0]
+                        
+                        let savePath = documentDir + "/bach.jpg"
+                        
+                        do {
+                            
+                            try UIImageJPEGRepresentation(self.bachImage.image!, 1)?.write(to: URL(fileURLWithPath: savePath))
+                        } catch {
+                            
+                        }
+                        
+                        
+                    }
+                    
+                    
+                }
+                
+                
+            }
+            
+        }
+        task.resume()
+        
     }
-
+    
 
 }
-
